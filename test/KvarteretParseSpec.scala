14917549430b7c641cbe4f7eb73dbe808a79b761
Mads André Lundeland <lundeland@gmail.com>
2014-01-17 16:35:29 +0100

    Test av parsing

--- test/KvarteretParseSpec.scala
+++ test/KvarteretParseSpec.scala
@@ -6,6 +6,8 @@ import org.specs2.runner._
 import org.junit.runner._
 
 import play.api.test._
+import scala.util.matching.Regex
+import scala.util.matching.Regex.MatchIterator
 
 @RunWith(classOf[JUnitRunner])
 class KvarteretParseSpec extends Specification {
@@ -15,6 +17,15 @@ class KvarteretParseSpec extends Specification {
       val html: String = io.Source.fromInputStream(getClass.getResourceAsStream("test/kvarteret-source.html")).mkString
       val lemaitre: Event = Event(new SimpleDateFormat("yyyy-MM-dd").parse("2014-01-23"), "Lemâitre")
       Kvarteret.parse(html).contains(lemaitre)
+    }
+    "blah" in {
+      val str = "lørdag 21. januar 2014"
+      val pattern: Regex = """(\D+) (\d+)\. (\D+) (\d+)""".r
+      val m = pattern.findAllIn(str).matchData.next()
+      m.group(1) === "lørdag"
+      m.group(2).toInt === 21
+      m.group(3) === "januar"
+      m.group(4).toInt === 2014
     }
   }
 }
